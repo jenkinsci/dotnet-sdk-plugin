152a0598dad980f641bf37b3956b267d205a4f9a
Tim Van Holder <tim.vanholder@gmail.com>
2020-05-20 00:12:18 +0200

    Add a DotNetListPackage builder, with full UI.

---
+++ src/main/java/io/jenkins/plugins/dotnet/DotNetListPackage.java
@@ -0,0 +1,47 @@
+package io.jenkins.plugins.dotnet;
+
+import edu.umd.cs.findbugs.annotations.NonNull;
+import hudson.Extension;
+import hudson.model.AbstractProject;
+import org.jenkinsci.Symbol;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+import java.util.List;
+
+/** A build step using the 'dotnet' executable to list all resolved package dependencies for a project. */
+public final class DotNetListPackage extends DotNet {
+
+  @DataBoundConstructor
+  public DotNetListPackage() {
+  }
+
+  @Override
+  protected void addCommandLineArguments(@NonNull List<String> args) {
+    args.add("list");
+    args.add("package");
+  }
+
+  @Extension
+  @Symbol("dotnetListPackage")
+  public static class DescriptorImpl extends DotNet.DescriptorImpl {
+
+    public DescriptorImpl() {
+      load();
+    }
+
+    protected DescriptorImpl(Class<? extends DotNetListPackage> clazz) {
+      super(clazz);
+    }
+
+    public boolean isApplicable(Class<? extends AbstractProject> jobType) {
+      return true;
+    }
+
+    @NonNull
+    public String getDisplayName() {
+      return Messages.DotNetListPackage_DisplayName();
+    }
+
+  }
+
+}
