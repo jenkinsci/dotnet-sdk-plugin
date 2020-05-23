1957400b9797a405f69bf30d0dcb5d6581f9f9dc
Tim Van Holder <tim.vanholder@gmail.com>
2020-05-23 23:33:53 +0200

    Implemented a tool installer.
    This uses a JSON file containing the available SDKs installation
    packages. Currently this is a hand-made file; I did't find an official
    file from Microsoft. It's also incomplete (it gets tedious; a dozen
    releases, each with a handful of feature band SDKs, for which there are
    8 or so installation packages).
    
    The existing DowloadFromUrlInstaller is sadly not applicable - it is
    specifically intended for cases where the download package is not
    platform-specific. So this installer mixes bits of that with the more
    generic ZipExtractionInstaller and adds a convenient UI for selecting
    the installation package to use.

---
+++ src/main/java/io/jenkins/plugins/dotnet/DotNetSDKInstaller.java
@@ -0,0 +1,213 @@
+package io.jenkins.plugins.dotnet;
+
+import edu.umd.cs.findbugs.annotations.NonNull;
+import hudson.Extension;
+import hudson.FilePath;
+import hudson.Util;
+import hudson.model.Node;
+import hudson.model.TaskListener;
+import hudson.tools.ToolInstallation;
+import hudson.tools.ToolInstaller;
+import hudson.tools.ToolInstallerDescriptor;
+import hudson.util.FormValidation;
+import hudson.util.ListBoxModel;
+import io.jenkins.plugins.dotnet.data.Downloads;
+import org.jenkinsci.Symbol;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.DataBoundSetter;
+import org.kohsuke.stapler.QueryParameter;
+
+import java.io.IOException;
+import java.net.URL;
+
+public final class DotNetSDKInstaller extends ToolInstaller {
+
+  @DataBoundConstructor
+  public DotNetSDKInstaller(String label) {
+    super(label);
+  }
+
+  @Override
+  public FilePath performInstallation(ToolInstallation tool, Node node, TaskListener log) throws IOException, InterruptedException {
+    final FilePath dir = this.preferredLocation(tool, node);
+    {
+      final FilePath marker = dir.child(".installedFrom");
+      // FIXME: readToString() does not allow specifying an encoding, but we wrote in UTF-8 specifically.
+      if (marker.exists() && marker.readToString().equals(this.url))
+        return dir;
+    }
+    if (dir.installIfNecessaryFrom(new URL(this.url), log, Messages.DotNetSDKInstaller_Installing(this.url, dir, node.getDisplayName()))) {
+      dir.child(".timestamp").delete();
+      dir.child(".installedFrom").write(this.url,"UTF-8");
+      // For now, this does no include ZipExtractionInstaller's behaviour of making everything executable (in part because it's
+      // not exposed, so I can't directly reuse it).
+      // TODO: Maybe save the sdk name, so that it does not need to be looked up (in the context of creating/updating global.json
+      // TODO: to force the use of an exact SDK version).
+    }
+    // For the time being, this does not replicate
+    return dir;
+  }
+
+  //region Properties
+
+  private boolean includeEndOfLife;
+
+  public boolean isIncludeEndOfLife() {
+    return this.includeEndOfLife;
+  }
+
+  @DataBoundSetter
+  public void setIncludeEndOfLife(boolean includeEndOfLife) {
+    this.includeEndOfLife = includeEndOfLife;
+  }
+
+  private boolean includePreview;
+
+  public boolean isIncludePreview() {
+    return this.includePreview;
+  }
+
+  @DataBoundSetter
+  public void setIncludePreview(boolean includePreview) {
+    this.includePreview = includePreview;
+  }
+
+  private String release;
+
+  public String getRelease() {
+    return this.release;
+  }
+
+  @DataBoundSetter
+  public void setRelease(String release) {
+    this.release = release;
+  }
+
+  private String sdk;
+
+  public String getSdk() {
+    return this.sdk;
+  }
+
+  @DataBoundSetter
+  public void setSdk(String sdk) {
+    this.sdk = sdk;
+  }
+
+  private String url;
+
+  public String getUrl() {
+    return this.url;
+  }
+
+  @DataBoundSetter
+  public void setUrl(String url) {
+    this.url = url;
+  }
+
+  private String version;
+
+  public String getVersion() {
+    return this.version;
+  }
+
+  @DataBoundSetter
+  public void setVersion(String version) {
+    this.version = version;
+  }
+
+  //endregion
+
+  //region DescriptorImpl
+
+  @Extension
+  @Symbol("installDotNetSDK")
+  public static final class DescriptorImpl extends ToolInstallerDescriptor<DotNetSDKInstaller> {
+
+    private ListBoxModel createList() {
+      final ListBoxModel model = new ListBoxModel();
+      // Setting the value to null does not work - it causes the validation routines to get '"null"', not 'null', as value.
+      model.add(Messages.DotNetSDKInstaller_NotSelected(), "");
+      return model;
+    }
+
+    public FormValidation doCheckRelease(@QueryParameter String version, @QueryParameter String release, @QueryParameter String value) {
+      if (Util.fixEmpty(version) == null)
+        return FormValidation.error(Messages.DotNetSDKInstaller_VersionRequired());
+      if (Util.fixEmpty(value) == null)
+        return FormValidation.error(Messages.DotNetSDKInstaller_Required());
+      if (Downloads.getInstance().getRelease(version, value) == null)
+        return FormValidation.error(Messages.DotNetSDKInstaller_InvalidRelease(value, version));
+      return FormValidation.ok();
+    }
+
+    public FormValidation doCheckSdk(@QueryParameter String version, @QueryParameter String release, @QueryParameter String value) {
+      if (Util.fixEmpty(version) == null)
+        return FormValidation.error(Messages.DotNetSDKInstaller_VersionRequired());
+      if (Util.fixEmpty(release) == null)
+        return FormValidation.error(Messages.DotNetSDKInstaller_ReleaseRequired());
+      if (Util.fixEmpty(value) == null)
+        return FormValidation.error(Messages.DotNetSDKInstaller_Required());
+      if (Downloads.getInstance().getSdk(version, release, value) == null)
+        return FormValidation.error(Messages.DotNetSDKInstaller_InvalidSdk(value, version, release));
+      return FormValidation.ok();
+    }
+
+    public FormValidation doCheckUrl(@QueryParameter String version, @QueryParameter String release, @QueryParameter String sdk, @QueryParameter String value) {
+      if (Util.fixEmpty(version) == null)
+        return FormValidation.error(Messages.DotNetSDKInstaller_VersionRequired());
+      if (Util.fixEmpty(release) == null)
+        return FormValidation.error(Messages.DotNetSDKInstaller_ReleaseRequired());
+      if (Util.fixEmpty(sdk) == null)
+        return FormValidation.error(Messages.DotNetSDKInstaller_SdkRequired());
+      if (Util.fixEmpty(value) == null)
+        return FormValidation.error(Messages.DotNetSDKInstaller_Required());
+      if (Downloads.getInstance().getPackage(version, release, sdk, value) == null)
+        return FormValidation.error(Messages.DotNetSDKInstaller_InvalidPackage(version, release, sdk));
+      return FormValidation.ok();
+    }
+
+    public FormValidation doCheckVersion(@QueryParameter String value) {
+      if (Util.fixEmpty(value) == null)
+        return FormValidation.error(Messages.DotNetSDKInstaller_Required());
+      if (Downloads.getInstance().getVersion(value) == null)
+        return FormValidation.error(Messages.DotNetSDKInstaller_InvalidVersion(value));
+      return FormValidation.ok();
+    }
+
+    public ListBoxModel doFillUrlItems(@QueryParameter String version, @QueryParameter String release, @QueryParameter String sdk) {
+      return Downloads.getInstance().addPackages(this.createList(), version, release, sdk);
+    }
+
+    public ListBoxModel doFillReleaseItems(@QueryParameter String version) {
+      return Downloads.getInstance().addReleases(this.createList(), version);
+    }
+
+    public ListBoxModel doFillSdkItems(@QueryParameter String version, @QueryParameter String release) {
+      return Downloads.getInstance().addSdks(this.createList(), version, release);
+    }
+
+    public ListBoxModel doFillVersionItems(@QueryParameter boolean includePreview, @QueryParameter boolean includeEndOfLife) {
+      return Downloads.getInstance().addVersions(this.createList(), includePreview, includeEndOfLife);
+    }
+
+    @NonNull
+    @Override
+    public String getDisplayName() {
+      return Messages.DotNetSDKInstaller_DisplayName();
+    }
+
+    @Override
+    public boolean isApplicable(Class<? extends ToolInstallation> toolType) {
+      return toolType == DotNetSDK.class;
+    }
+
+  }
+
+  //endregion
+
+  //region SDK Downloads Data Structure
+
+  //endregion
+
+}
---
+++ src/main/java/io/jenkins/plugins/dotnet/data/Downloads.java
@@ -0,0 +1,329 @@
+package io.jenkins.plugins.dotnet.data;
+
+import edu.umd.cs.findbugs.annotations.NonNull;
+import hudson.util.ListBoxModel;
+import io.jenkins.plugins.dotnet.Messages;
+import net.sf.ezmorph.Morpher;
+import net.sf.json.util.EnumMorpher;
+import net.sf.json.util.JSONUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public final class Downloads {
+
+  private static Downloads instance = null;
+
+  public static synchronized Downloads getInstance() {
+    if (Downloads.instance == null) {
+      { // Explicit morpher fiddling to avoid a warning being logged
+        final Morpher statusMorpher = new EnumMorpher(Version.Status.class);
+        JSONUtils.getMorpherRegistry().registerMorpher(statusMorpher);
+        Downloads.instance = Data.loadJson(Downloads.class, Downloads.class);
+        JSONUtils.getMorpherRegistry().deregisterMorpher(statusMorpher);
+      }
+      if (Downloads.instance == null)
+        Downloads.instance = new Downloads();
+      else
+        Downloads.instance.finish();
+    }
+    return Downloads.instance;
+  }
+
+  public Version[] versions = null;
+
+  private final Map<String, Version> versionMap = new HashMap<>();
+
+  private void finish() {
+    if (!this.versionMap.isEmpty())
+      this.versionMap.clear();
+    if (this.versions == null)
+      return;
+    for (final Version v : this.versions) {
+      v.finish();
+      this.versionMap.put(v.name, v);
+    }
+  }
+
+  //region Lookup Methods
+
+  public Package getPackage(String version, String release, String sdk, String url) {
+    final Version v = this.getVersion(version);
+    if (v == null)
+      return null;
+    final Release r = v.getRelease(release);
+    if (r == null)
+      return null;
+    final Sdk s = r.getSdk(sdk);
+    if (s == null)
+      return null;
+    return s.getPackage(url);
+  }
+
+  public Release getRelease(String version, String release) {
+    final Version v = this.getVersion(version);
+    if (v == null)
+      return null;
+    return v.getRelease(release);
+  }
+
+  public Sdk getSdk(String version, String release, String sdk) {
+    final Version v = this.getVersion(version);
+    if (v == null)
+      return null;
+    final Release r = v.getRelease(release);
+    if (r == null)
+      return null;
+    return r.getSdk(sdk);
+  }
+
+  public Version getVersion(String name) {
+    return this.versionMap.get(name);
+  }
+
+  //endregion
+
+  //region Listbox Methods
+
+  public ListBoxModel addPackages(@NonNull ListBoxModel model, String version, String release, String sdk) {
+    final Sdk s = this.getSdk(version, release, sdk);
+    if (s != null && s.packages != null) {
+      for (Package p : s.packages)
+        model.add(p.getDisplayName(), p.url);
+    }
+    return model;
+  }
+
+  public ListBoxModel addReleases(@NonNull ListBoxModel model, String version) {
+    final Version v = this.getVersion(version);
+    if (v != null && v.releases != null) {
+      for (Release r : v.releases)
+        model.add(r.getDisplayName(), r.name);
+    }
+    return model;
+  }
+
+  public ListBoxModel addSdks(@NonNull ListBoxModel model, String version, String release) {
+    final Release r = this.getRelease(version, release);
+    if (r != null && r.sdks != null) {
+      for (Sdk s : r.sdks)
+        model.add(s.getDisplayName(), s.name);
+    }
+    return model;
+  }
+
+  public ListBoxModel addVersions(@NonNull ListBoxModel model, boolean includePreview, boolean includeEndOfLife) {
+    if (this.versions != null) {
+      for (Version v : this.versions) {
+        if (v.status == Version.Status.PREVIEW && !includePreview)
+          continue;
+        if (v.status == Version.Status.END_OF_LIFE && !includeEndOfLife)
+          continue;
+        model.add(v.getDisplayName(), v.name);
+      }
+    }
+    return model;
+  }
+
+  //endregion
+
+  //region Nested Types
+
+  //region Version
+
+  public static final class Version {
+
+    //region Status Enum
+
+    public enum Status {
+
+      CURRENT,
+
+      END_OF_LIFE,
+
+      LTS,
+
+      MAINTENANCE,
+
+      PREVIEW,
+
+      UNKNOWN,
+
+      ;
+
+      public String getDisplayName() {
+        switch (this) {
+          case CURRENT:
+            return Messages.Downloads_Version_Status_Current();
+          case END_OF_LIFE:
+            return Messages.Downloads_Version_Status_EndOfLife();
+          case LTS:
+            return Messages.Downloads_Version_Status_LTS();
+          case MAINTENANCE:
+            return Messages.Downloads_Version_Status_Maintenance();
+          case PREVIEW:
+            return Messages.Downloads_Version_Status_Preview();
+          case UNKNOWN:
+            return Messages.Downloads_Version_Status_Unknown();
+        }
+        return null;
+      }
+
+    }
+
+    //endregion
+
+    public String name;
+
+    public Status status;
+
+    public String endOfSupport;
+
+    public Release[] releases;
+
+    private final Map<String, Release> releaseMap = new HashMap<>();
+
+    private void finish() {
+      if (this.name == null)
+        this.name = Messages.Downloads_Unknown();
+      if (this.status == null)
+        this.status = Status.UNKNOWN;
+      if (!this.releaseMap.isEmpty())
+        this.releaseMap.clear();
+      if (this.releases == null)
+        return;
+      for (final Release r : this.releases) {
+        r.finish();
+        this.releaseMap.put(r.name, r);
+      }
+    }
+
+    @NonNull
+    public String getDisplayName() {
+      if (this.endOfSupport != null)
+        return Messages.Downloads_Version_DisplayNameWithDate(this.name, this.status.getDisplayName(), this.endOfSupport);
+      else
+        return Messages.Downloads_Version_DisplayName(this.name, this.status.getDisplayName());
+    }
+
+    public Release getRelease(String name) {
+      return this.releaseMap.get(name);
+    }
+
+  }
+
+  //endregion
+
+  //region Release
+
+  public static final class Release {
+
+    public String name;
+
+    public String released;
+
+    public boolean securityFixes;
+
+    public Sdk[] sdks;
+
+    private final Map<String, Sdk> sdkMap = new HashMap<>();
+
+    private void finish() {
+      if (this.name == null)
+        this.name = Messages.Downloads_Unknown();
+      if (this.released == null)
+        this.released = Messages.Downloads_Unknown();
+      if (!this.sdkMap.isEmpty())
+        this.sdkMap.clear();
+      if (this.sdks == null)
+        return;
+      for (final Sdk s : this.sdks) {
+        s.finish();
+        this.sdkMap.put(s.name, s);
+      }
+    }
+
+    @NonNull
+    public String getDisplayName() {
+      if (this.securityFixes)
+        return Messages.Downloads_Release_DisplayNameWithSecurity(this.name, this.released);
+      return Messages.Downloads_Release_DisplayName(this.name, this.released);
+    }
+
+    public Sdk getSdk(String name) {
+      return this.sdkMap.get(name);
+    }
+
+  }
+
+  //endregion
+
+  //region Sdk
+
+  public static final class Sdk {
+
+    public String name;
+
+    public String vs;
+
+    public Package[] packages;
+
+    private final Map<String, Package> packageMap = new HashMap<>();
+
+    private void finish() {
+      if (this.name == null)
+        this.name = Messages.Downloads_Unknown();
+      if (this.vs == null)
+        this.vs = Messages.Downloads_Unknown();
+      if (!this.packageMap.isEmpty())
+        this.packageMap.clear();
+      if (this.packages == null)
+        return;
+      for (final Package p : this.packages) {
+        p.finish();
+        this.packageMap.put(p.url, p);
+      }
+    }
+
+    @NonNull
+    public String getDisplayName() {
+      return Messages.Downloads_Sdk_DisplayName(this.name, this.vs);
+    }
+
+    public Package getPackage(String url) {
+      return this.packageMap.get(url);
+    }
+
+  }
+
+  //endregion
+
+  //region Package
+
+  public static final class Package {
+
+    public String platform;
+
+    public String architecture;
+
+    public String url;
+
+    private void finish() {
+      if (this.platform == null)
+        this.platform = Messages.Downloads_Unknown();
+      if (this.architecture == null)
+        this.architecture = Messages.Downloads_Unknown();
+    }
+
+    @NonNull
+    public String getDisplayName() {
+      return Messages.Downloads_Package_DisplayName(this.platform, this.architecture);
+    }
+
+  }
+
+  //endregion
+
+  //endregion
+
+}
