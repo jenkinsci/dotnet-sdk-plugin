11d6d1fa4617e006757622c32cd38b23feeb116c
Tim Van Holder <tim.vanholder@gmail.com>
2020-08-18 22:46:56 +0200

    Add all test methods for Clean

--- src/test/java/io/jenkins/plugins/dotnet/commands/msbuild/CleanTests.java
+++ src/test/java/io/jenkins/plugins/dotnet/commands/msbuild/CleanTests.java
@@ -10,4 +10,47 @@ public final class CleanTests extends CommandTests {
     super.runCommandAndValidateProcessExecution(Clean::new, check -> check.expectCommand().withArgument("clean"));
   }
 
+  private static final String PROJECT = "Foo.Bar.sln";
+
+  @Test
+  public void normalExecutionWorks() throws Exception {
+    super.runCommandAndValidateProcessExecution(() -> {
+      final Clean command = new Clean();
+      command.setProject(CleanTests.PROJECT);
+      return command;
+    }, check -> check.expectCommand().withArguments("clean", CleanTests.PROJECT));
+  }
+
+  private static final String FRAMEWORK = "net5.0";
+
+  @Test
+  public void frameworkOptionWorks() throws Exception {
+    super.runCommandAndValidateProcessExecution(() -> {
+      final Clean command = new Clean();
+      command.setFramework(CleanTests.FRAMEWORK);
+      return command;
+    }, check -> check.expectCommand().withArguments("clean", "-f:" + CleanTests.FRAMEWORK));
+    super.runCommandAndValidateProcessExecution(() -> {
+      final Clean command = new Clean();
+      command.setFramework("  ");
+      return command;
+    }, check -> check.expectCommand().withArguments("clean"));
+  }
+
+  private static final String RUNTIME = "debian-x64";
+
+  @Test
+  public void runtimeOptionWorks() throws Exception {
+    super.runCommandAndValidateProcessExecution(() -> {
+      final Clean command = new Clean();
+      command.setRuntime(CleanTests.RUNTIME);
+      return command;
+    }, check -> check.expectCommand().withArguments("clean", "-r:" + CleanTests.RUNTIME));
+    super.runCommandAndValidateProcessExecution(() -> {
+      final Clean command = new Clean();
+      command.setRuntime(null);
+      return command;
+    }, check -> check.expectCommand().withArguments("clean"));
+  }
+
 }
