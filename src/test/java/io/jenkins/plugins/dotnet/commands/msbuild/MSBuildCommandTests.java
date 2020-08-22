5c99adcde99f2176fa6beaa7334aeab9508a27e7
Tim Van Holder <tim.vanholder@gmail.com>
2020-08-22 16:12:54 +0200

    Add all test methods for MSBuildCommand

--- src/test/java/io/jenkins/plugins/dotnet/commands/msbuild/MSBuildCommandTests.java
+++ src/test/java/io/jenkins/plugins/dotnet/commands/msbuild/MSBuildCommandTests.java
@@ -1,5 +1,6 @@
 package io.jenkins.plugins.dotnet.commands.msbuild;
 
+import hudson.Util;
 import io.jenkins.plugins.dotnet.commands.CommandTests;
 import org.junit.Test;
 
@@ -8,6 +9,101 @@ public final class MSBuildCommandTests extends CommandTests {
   @Test
   public void simpleExecutionWorks() throws Exception {
     super.runCommandAndValidateProcessExecution(MSBuildCommand::new, CommandLineChecker::expectCommand);
+  }
+
+  private static final String PROJECT = "Foo.Bar.sln";
+
+  @Test
+  public void normalExecutionWorks() throws Exception {
+    super.runCommandAndValidateProcessExecution(() -> {
+      final MSBuildCommand command = new MSBuildCommand("unit-test");
+      command.setProject(MSBuildCommandTests.PROJECT);
+      return command;
+    }, check -> check.expectCommand().withArguments("unit-test", MSBuildCommandTests.PROJECT));
+  }
+
+  private static final String CONFIGURATION = "Release";
+
+  @Test
+  public void configurationOptionWorks() throws Exception {
+    super.runCommandAndValidateProcessExecution(() -> {
+      final MSBuildCommand command = new MSBuildCommand();
+      command.setConfiguration(MSBuildCommandTests.CONFIGURATION);
+      return command;
+    }, check -> check.expectCommand().withArgument("-c:" + MSBuildCommandTests.CONFIGURATION));
+    super.runCommandAndValidateProcessExecution(() -> {
+      final MSBuildCommand command = new MSBuildCommand();
+      command.setConfiguration(null);
+      return command;
+    }, CommandLineChecker::expectCommand);
+  }
+
+  @Test
+  public void nologoFlagWorks() throws Exception {
+    super.runCommandAndValidateProcessExecution(() -> {
+      final MSBuildCommand command = new MSBuildCommand();
+      command.setNologo(true);
+      return command;
+    }, commandLineChecker -> commandLineChecker.expectCommand().withArguments("--nologo"));
+    super.runCommandAndValidateProcessExecution(() -> {
+      final MSBuildCommand command = new MSBuildCommand();
+      command.setNologo(false);
+      return command;
+    }, CommandLineChecker::expectCommand);
+  }
+
+  private static final String OPTIONS = "--random -o:ptions 'Including Whatever'";
+
+  @Test
+  public void optionsOptionWorks() throws Exception {
+    super.runCommandAndValidateProcessExecution(() -> {
+      final MSBuildCommand command = new MSBuildCommand();
+      command.setOptions(MSBuildCommandTests.OPTIONS);
+      return command;
+    }, check -> check.expectCommand().withArguments(Util.tokenize(MSBuildCommandTests.OPTIONS)));
+    super.runCommandAndValidateProcessExecution(() -> {
+      final MSBuildCommand command = new MSBuildCommand();
+      command.setOptions(null);
+      return command;
+    }, CommandLineChecker::expectCommand);
+  }
+
+  private static final String OUTPUT_DIRECTORY = "/path/to/output";
+
+  @Test
+  public void outputDirectoryOptionWorks() throws Exception {
+    super.runCommandAndValidateProcessExecution(() -> {
+      final MSBuildCommand command = new MSBuildCommand();
+      command.setOutputDirectory(MSBuildCommandTests.OUTPUT_DIRECTORY);
+      return command;
+    }, check -> check.expectCommand().withArguments("--output", MSBuildCommandTests.OUTPUT_DIRECTORY));
+    super.runCommandAndValidateProcessExecution(() -> {
+      final MSBuildCommand command = new MSBuildCommand();
+      command.setOutputDirectory(null);
+      return command;
+    }, CommandLineChecker::expectCommand);
+  }
+
+  private static final String PROPERTIES = "# A comment\n" +
+    "\n" +
+    "# That was a blank line\n" +
+    "MyProperty=Value \\\n" +
+    " Containing=An Equals Sign\n" +
+    "# A Comment";
+
+  @Test
+  public void propertiesOptionWorks() throws Exception {
+    // Note: this cannot test a string with multiple properties, because their order in the result cannot be guaranteed.
+    super.runCommandAndValidateProcessExecution(() -> {
+      final MSBuildCommand command = new MSBuildCommand();
+      command.setProperties(MSBuildCommandTests.PROPERTIES);
+      return command;
+    }, check -> check.expectCommand().withArgument("-p:MyProperty=Value Containing=An Equals Sign"));
+    super.runCommandAndValidateProcessExecution(() -> {
+      final MSBuildCommand command = new MSBuildCommand();
+      command.setProperties(null);
+      return command;
+    }, CommandLineChecker::expectCommand);
   }
 
   @Test
@@ -23,6 +119,22 @@ public final class MSBuildCommandTests extends CommandTests {
     super.runCommandAndValidateProcessExecution(() -> {
       final MSBuildCommand command = new MSBuildCommand();
       command.setShutDownBuildServers(false);
+      return command;
+    }, CommandLineChecker::expectCommand);
+  }
+
+  private static final String VERBOSITY = "minimal";
+
+  @Test
+  public void verbosityOptionWorks() throws Exception {
+    super.runCommandAndValidateProcessExecution(() -> {
+      final MSBuildCommand command = new MSBuildCommand();
+      command.setVerbosity(MSBuildCommandTests.VERBOSITY);
+      return command;
+    }, check -> check.expectCommand().withArgument("-v:" + MSBuildCommandTests.VERBOSITY));
+    super.runCommandAndValidateProcessExecution(() -> {
+      final MSBuildCommand command = new MSBuildCommand();
+      command.setVerbosity(null);
       return command;
     }, CommandLineChecker::expectCommand);
   }
