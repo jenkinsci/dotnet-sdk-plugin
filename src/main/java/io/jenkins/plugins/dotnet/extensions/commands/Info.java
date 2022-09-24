21c76ef9f080894f537b36138a7263462537c89b
Tim Van Holder <tim.vanholder@gmail.com>
2022-09-24 15:04:31 +0200

    Extend the `Info` command
    It can now be used for `--list-runtimes`, `--list-sdks` and `--version`
    instead of `--info`.

--- src/main/java/io/jenkins/plugins/dotnet/extensions/commands/Info.java
+++ src/main/java/io/jenkins/plugins/dotnet/extensions/commands/Info.java
@@ -1,11 +1,18 @@
 package io.jenkins.plugins.dotnet.extensions.commands;
 
+import edu.umd.cs.findbugs.annotations.CheckForNull;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import hudson.AbortException;
 import hudson.Extension;
+import hudson.model.Item;
+import hudson.util.FormValidation;
 import io.jenkins.plugins.dotnet.DotNetArguments;
 import org.jenkinsci.Symbol;
+import org.kohsuke.stapler.AncestorInPath;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.DataBoundSetter;
+import org.kohsuke.stapler.QueryParameter;
+import org.kohsuke.stapler.verb.POST;
 
 public class Info extends Command {
 
@@ -17,8 +24,56 @@ public class Info extends Command {
 
   @Override
   public void addCommandLineArguments(@NonNull DotNetArguments arguments) throws AbortException {
-    arguments.addFlag("info");
+    if (this.listRuntimes) {
+      arguments.addFlag("list-runtimes");
+    }
+    else if (this.listSdks) {
+      arguments.addFlag("list-sdks");
+    }
+    else if (this.showVersion) {
+      arguments.addFlag("version");
+    }
+    else {
+      arguments.addFlag("info");
+    }
   }
+
+  //region Properties
+
+  private boolean listRuntimes;
+
+  public boolean isListRuntimes() {
+    return this.listRuntimes;
+  }
+
+  @DataBoundSetter
+  public void setListRuntimes(boolean listRuntimes) {
+    this.listRuntimes = listRuntimes;
+  }
+
+  private boolean listSdks;
+
+  public boolean isListSdks() {
+    return this.listSdks;
+  }
+
+  @DataBoundSetter
+  public void setListSdks(boolean listSdks) {
+    this.listSdks = listSdks;
+  }
+
+  private boolean showVersion;
+
+  public boolean isShowVersionOnly() {
+    return this.showVersion;
+  }
+
+  @DataBoundSetter
+  public void setShowVersionOnly(boolean showVersion) {
+    this.showVersion = showVersion;
+  }
+
+  //endregion
 
   @Extension
   @Symbol("info")
@@ -28,6 +83,45 @@ public class Info extends Command {
     @Override
     public String getDisplayName() {
       return Messages.Info_DisplayName();
+    }
+
+    @NonNull
+    @POST
+    public FormValidation doCheckListRuntimes(@QueryParameter boolean listRuntimes, @QueryParameter boolean listSdks,
+                                              @QueryParameter boolean showVersion, @CheckForNull @AncestorInPath Item item) {
+      if (item != null) {
+        item.checkPermission(Item.CONFIGURE);
+      }
+      if (listRuntimes && (listSdks || showVersion)) {
+        return FormValidation.error(Messages.Info_MutuallyExclusive());
+      }
+      return FormValidation.ok();
+    }
+
+    @NonNull
+    @POST
+    public FormValidation doCheckListSdks(@QueryParameter boolean listRuntimes, @QueryParameter boolean listSdks,
+                                          @QueryParameter boolean showVersion, @CheckForNull @AncestorInPath Item item) {
+      if (item != null) {
+        item.checkPermission(Item.CONFIGURE);
+      }
+      if (listSdks && (listRuntimes || showVersion)) {
+        return FormValidation.error(Messages.Info_MutuallyExclusive());
+      }
+      return FormValidation.ok();
+    }
+
+    @NonNull
+    @POST
+    public FormValidation doCheckShowVersion(@QueryParameter boolean listRuntimes, @QueryParameter boolean listSdks,
+                                             @QueryParameter boolean showVersion, @CheckForNull @AncestorInPath Item item) {
+      if (item != null) {
+        item.checkPermission(Item.CONFIGURE);
+      }
+      if (showVersion && (listRuntimes || listSdks)) {
+        return FormValidation.error(Messages.Info_MutuallyExclusive());
+      }
+      return FormValidation.ok();
     }
 
   }
